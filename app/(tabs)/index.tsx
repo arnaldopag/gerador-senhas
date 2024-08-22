df1df4572fa7fcf22a1a5394fc854975adff8faa
Arnaldo Pagani Junior <arnaldopaganijr@gmail.com>
2024-08-21 21:10:18 -0300

    alterando o Readme.md

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,98 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { useState } from 'react'
+import { ModalPassword } from '/Users/Arnaldo/Desktop/codigo/Senac/gerador-senhas/src/components/modal'
+import Slider from '@react-native-community/slider'
+import { View, Text, StyleSheet, Image, TouchableOpacity, Modal } from 'react-native'
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?";
+const letrasMaiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const letrasMinusculas = "abcdefghijklmnopqrstuvwxyz";
+const numeros = "0123456789";
+const simbolos = "!@#$%^&*()_+[]{}|;:,.<>?";
 
-export default function HomeScreen() {
+
+
+export default function Index() {
+
+  const [size, setSize] = useState(8);
+  const [passwordValue, setPasswordValue] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
+
+  function generatePassword() {
+    let password = ''
+    for (let i = 0, n = charSet.length; i < size; i++) {
+      password += charSet.charAt(Math.floor(Math.random() * n))
+    }
+    setPasswordValue(password);
+    setModalVisible(true);
+    console.log(password)
+  }
+
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <View style={styles.container}>
+      <Image
+        source={require("/Users/Arnaldo/Desktop/codigo/Senac/gerador-senhas/src/assets/logo.png")}
+        style={styles.logo}
+      />
+      <Text style={styles.title}>{size} caracteres</Text>
+      <View style={styles.area}>
+        <Slider
+          style={{ height: 50 }}
+          minimumValue={6}
+          maximumValue={30}
+          maximumTrackTintColor='#ff0000'
+          minimumTrackTintColor='#000'
+          thumbTintColor='#392de9'
+          value={size}
+          onValueChange={(value) =>
+            setSize(Math.round(value))}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+      </View>
+      <TouchableOpacity style={styles.button} onPress={generatePassword}>
+        <Text style={styles.buttonText}>Gerar Senha</Text>
+      </TouchableOpacity>
+
+
+      <Modal visible={modalVisible} animationType='fade' transparent={true}>
+        <ModalPassword />
+      </Modal>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    backgroundColor: "#F3F3FF",
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  logo: {
+    marginBottom: 60
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  area: {
+    marginTop: 14,
+    marginBottom: 14,
+    width: "80%",
+    backgroundColor: "#FFF",
+    borderRadius: 8,
+    padding: 6
   },
-});
+  button: {
+    backgroundColor: "#392de9",
+    width: "80%",
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+    marginBottom: 18
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 20
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+})
